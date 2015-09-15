650a1a9562524df4db2e7d81b1441277c4a90691
Tim <Tim@Tim-HP.home>
2015-09-14 20:29:04 -0400

    initial

---
+++ TheBasics/src/StartingPoint.java
@@ -0,0 +1,14 @@
+
+public class StartingPoint {
+	public static void main(String trav[]){
+		
+		int favNum;
+		String words;
+		
+		favNum = 7;
+		words = "My favorite number is ";
+		
+		System.out.print(words + favNum + ".");
+		
+	}
+}
---
+++ TheBasics/src/SyntaxError.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SyntaxError
+{
+	public static void main(String[] args)
+	{
+		System.out.println("The product is");
+		Scanner keyboard = new Scanner(System.in);
+		n1 = keyboard.nextInt();
+		n2 = keyboard.nextInt();
+		int product = n1 * n2;
+		System.out.println("The product is" + product);
+		
+	}
+	}
+
+
