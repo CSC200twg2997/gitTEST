eb0702a9762bb3c854e0a53cc9b1718344e1a410
Tim <Tim@Tim-HP.home>
2015-09-14 20:23:01 -0400

    initial

---
+++ Scanner/src/HelloMyName.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; 
+ 
+public class HelloMyName {
+
+ 
+ 	public static void main(String[] args) { 
+ 		//instantiation		 
+ 		Scanner keyboard = new Scanner(System.in); 
+ 		System.out.println("Enter your username."); 
+ 		String userName = keyboard.nextLine(); 
+ 
+ 
+ 		System.out.println("Enter your password."); 
+ 		String password = keyboard.nextLine(); 
+ 		System.out.print("Hello " + userName + ", "); 
+ 		System.out.println("welcome to CSC-200!\nYour password is " + password + "."); 
+ 		keyboard.nextLine(); 
+ 		keyboard.close(); 
+
+ 	}
+
+}
